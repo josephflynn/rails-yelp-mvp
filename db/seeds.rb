02bb9db489c3ba708ea33841851f43046ef96ef3
Joseph Flynn <joseph.f.flynn@gmail.com>
2023-05-24 16:51:55 +0100

    yelp done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+10.times do |restaurant|
+  restaurant = Restaurant.create(name: Faker::Restaurant.name , address: Faker::Address.full_address, category: CATEGORIES.sample, phone_number: Faker::PhoneNumber.phone_number)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished creating restuarants!"
+
+puts "Creating reviews..."
+
+40.times do |review|
+  review = Review.create(rating: rand(1..5), content: Faker::Restaurant.review, restaurant: Restaurant.all.sample)
+  puts "Created #{review.rating}"
+end
+puts "All Done!"
